ffd6b83d548a127c123e15d2750d7dd08877d82b
MasterP2 <arikpopeace98@gmail.com>
2023-02-21 22:13:51 -0500

    C# Prep 1

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("This is in C#");
+
+        Console.Write("What is your favorite color?");
+        string color = Console.ReadLine();
+        // Console.WriteLine("Your color is " + color);
+        Console.WriteLine($"Your color is {color}");
+
     }
 }
