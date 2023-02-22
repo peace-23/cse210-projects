3c9ee9a06e83e2743fc89270a06d6244a2625cbf
MasterP2 <arikpopeace98@gmail.com>
2023-02-21 22:17:43 -0500

    Update Program.cs

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,11 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your first name? ");
-        string fname = Console.ReadLine();
+        string first = Console.ReadLine();
 
         Console.Write("What is your last name? ");
-        string lname= Console.ReadLine();
-        
-        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
+        string last= Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
