47113dc6c82a97c290595d6218fb7dbdd3f87978
MasterP2 <arikpopeace98@gmail.com>
2023-03-01 21:28:14 -0500

    Added Learning Activity 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -3,7 +3,27 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "IT Support Specialist";
+        job1._company = "Apple";
+        job1._startYear = 2021;
+        job1._endYear= 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Delivery Driver";
+        job2._company = "Amazon";
+        job2._startYear = 2020;
+        job2._endYear= 2021;
+
+
+        Resume myResume = new Resume();
+        myResume._name = "Peace Arikpo";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
