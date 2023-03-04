c48dd151e207254b4154330b5f47e6755a9cca52
MasterP2 <arikpopeace98@gmail.com>
2023-03-04 11:35:37 -0500

    Updates Program.cs

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,12 +1,21 @@
-class PromptGenerator
+using System;
+
+public class PromptGenerator
 {
-    private List<string> prompts = new List<string>()
+    static List<string> prompts = new List<string>()
     {
         "What is one thing that made you proud of yourself today?",
         "What did you learned today?",
         "What are you most grateful for in your life right now?",
         "What is something you've always wanted to learn but haven't yet?",
-        "What are you looking forward to tomorrow?"
+        "What was the highlight of your day?",
+        "What was the most challenging part of your day?",
+        "What are some things you can do to take care of yourself today?",
+        "What is something you're looking forward to this week?",
+        "What is one thing that you would like to change about your daily routine?",
+        "What is something that inspires you?",
+        "What is one act of kindness you witnessed or received today?"
+
     };
 
     public string GetRandomPrompt()
