38f129b0fcc30fa6c3ac0b4da19d08ff8fb036ab
MasterP2 <arikpopeace98@gmail.com>
2023-03-22 22:25:08 -0400

    Added BreathingActivity : Activities

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Collections.Generic;
+using System.Threading;
 
-namespace BreathingActivity
+
+
+public class BreathingActivity : Activities
+// public class BreathingActivity
 {
-    public class BreathingActivity : Act
+    private int _duration;
+
+    public BreathingActivity(int duration)
     {
-        private DateTime ;
+        _duration = duration;
+    }
+
+    public void Start()
+    {
+        Console.Clear();
+        Console.WriteLine("Breathing Activity");
+
+        Console.WriteLine("This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.");
+        PauseWithAnimation(5);
+
+        for (int i = 0; i < _duration; i++)
+        {
+            Console.WriteLine("Breathe in...");
+            PauseWithAnimation(2);
+
+            Console.WriteLine("Breathe out...");
+            PauseWithAnimation(2);
+        }
+
+        Console.WriteLine("Good job! You have completed the Breathing Activity.");
+        Console.WriteLine("Duration: {0} seconds", _duration);
+        PauseWithAnimation(3);
+    }
+
+    private void PauseWithAnimation(int seconds)
+    {
+        for (int i = 0; i < seconds; i++)
+        {
+            Console.Write(".");
+            Thread.Sleep(1000);
+        }
+        Console.WriteLine();
     }
 }
