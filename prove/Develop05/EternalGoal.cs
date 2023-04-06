68c836f4ca612dc3d2ab98e51920625e37d6549c
MasterP2 <arikpopeace98@gmail.com>
2023-04-05 23:45:59 -0400

    Deleted Commented section

--- prove/Develop05/EternalGoal.cs
+++ prove/Develop05/EternalGoal.cs
@@ -1,30 +1,3 @@
-// using System;
-
-// namespace Develop06
-// {
-//     class EternalGoal : Goal
-//     {
-//         public EternalGoal(string name, string description, int value)
-//             : base(name, description, value)
-//         {
-//         }
-
-//         public override string GetStringRepresentation() =>
-//             $"EternalGoal:{_name} | {_description} | {_value}";
-
-//         public override void MarkComplete()
-//         {
-//             // Do nothing
-//         }
-
-//         public override void DisplayEntry()
-//         {
-//             Console.Write($"[ ] {_name}= {_description}");
-//         }
-//     }
-// }
-
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
